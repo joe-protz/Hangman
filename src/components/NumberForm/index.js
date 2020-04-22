84cdc39a7925580091d1afb89c172d1b948e039b
joe10994 <joe10994@gmail.com>
2020-04-22 13:15:49 -0400

    Use useRef hook to focus on inputs on component mounts
    - setTimeout was needed because when focusing during animation, animations
      would break.
    - Also removed autocomplete from form, as it as distracting

--- src/components/NumberForm/index.js
+++ src/components/NumberForm/index.js
@@ -2,24 +2,36 @@ import React from 'react'
 import Form from 'react-bootstrap/Form'
 import { PrimaryButton } from '../Shared/Styled'
 
-const NumberForm = ({ handleChange, handleSubmit, number }) => (
-  <span>
-    <Form
-      style={{ margin: '2rem auto .3rem', width: '60%' }}
-      onSubmit={handleSubmit}
-    >
-      <Form.Control
-        className="primary-form"
-        name="number"
-        placeholder="How many guesses are allowed?"
-        value={number}
-        onChange={handleChange}
-        type="number"
-        max="20"
-        min="1"
-      />
-      <PrimaryButton type="submit">Submit</PrimaryButton>
-    </Form>
-  </span>
-)
+const NumberForm = ({ handleChange, handleSubmit, number, reference }) => {
+  const inputs = [...Array(20).keys()].map(i => {
+    const incrementedI = i + 1
+    return (
+      <option key={incrementedI} value={incrementedI}>
+        {incrementedI}
+      </option>
+    )
+  })
+  return (
+    <span>
+      <Form
+        style={{ margin: '2rem auto .3rem', width: '60%' }}
+        onSubmit={handleSubmit}
+      >
+        <Form.Control
+          className="primary-form"
+          name="number"
+          placeholder="How many guesses are allowed?"
+          value={number}
+          onChange={handleChange}
+          as="select"
+          default="8"
+          ref={reference}
+        >
+          {inputs}
+        </Form.Control>
+        <PrimaryButton type="submit">Submit</PrimaryButton>
+      </Form>
+    </span>
+  )
+}
 export default NumberForm
